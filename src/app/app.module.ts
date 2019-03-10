0b3e36afb4d7e263c75547274f1a70f7e5c03bce
Namila Bandara <namila007@gmail.com>
2019-03-11 01:18:48 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContractComponent } from './contract/contract.component';
 import {MaterialModule} from './material-module';
-import { NewContractComponent } from './new-contract/new-contract.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContractComponent,
-    NewContractComponent
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
